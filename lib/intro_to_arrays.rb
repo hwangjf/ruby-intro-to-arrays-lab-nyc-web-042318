f9dde0a9a27b7676f3944c64255f37b4911c919e
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-15 14:15:58 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,5 +9,21 @@ def array_with_two_elements
 end
 
 def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods (array)
   return array.first
+end
+  
+def last_element_with_array_methods(array)
+  return array.last  
 end
